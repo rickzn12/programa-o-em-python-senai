b1bb9a9c3404414272d3a2a2708df05f6f480b15
rickzn12 <vieirarick08@gmail.com>
2026-07-17 21:50:01 -0300

    Add files via upload

---
+++ Aula-3/calculadora.py
@@ -0,0 +1,12 @@
+n1 = 20
+n2 = 4
+
+soma = n1 + n2 
+subtracao = n1 - n2 
+multiplicacao = n1 - n2
+divisao = n1 // n2
+
+print('soma', soma)
+print('subtracao', subtracao)
+print('multiplicacao', multiplicacao)
+print('divisao', divisao)
