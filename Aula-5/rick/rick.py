7dc258de2e6f2d3b6dc0c7175e9fdbce4380d8fd
rickzn12 <vieirarick08@gmail.com>
2026-07-22 20:08:58 -0300

    Add files via upload

---
+++ Aula-5/rick/rick.py
@@ -0,0 +1,73 @@
+#1 
+
+print("bem vindo")
+
+#2 
+
+verdadeiro = True 
+print(type(verdadeiro))
+
+#3
+
+n1 = 20
+n2 = 3
+
+resultado = n1 * n2 
+
+print("resultado da multiplicação", resultado)
+
+#4
+
+n1 = 40
+n2 = 6
+
+resultado = n1 / n2
+
+print("resultado da divisao", resultado)
+
+#5
+
+n1 = 90
+n2 = 45
+
+resultado = n1 - n2 
+
+print("resultado da subtração", resultado)
+
+#6 
+
+n1 = 60
+n2 = 6
+
+resultado = n1 // n2 
+
+print ("resultado divisao inteira", resultado)
+
+#7 
+
+n1 = 5
+n2 = 8
+n3 = 12
+n4 = 60
+
+resultado = n1 * n2 * n3 * n4 
+
+print ("resultado multiplicação", resultado)
+
+#8
+
+int(64)
+
+dobro = 64 * 2
+
+print(dobro)
+
+#9
+
+input("digite seu Nome:")
+input("digite seu Email:")
+input("digite sua Idade:")
+input("digite sua Cidade:")
+input("digite sua Graduação:")
+input("digite seu estado civil:")
+
