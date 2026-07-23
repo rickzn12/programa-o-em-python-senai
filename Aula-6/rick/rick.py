1c6118869b69a334f5388a758da2d52f217db5c6
rickzn12 <vieirarick08@gmail.com>
2026-07-23 19:29:32 -0300

    Add files via upload

---
+++ Aula-6/rick/rick.py
@@ -0,0 +1,49 @@
+#1
+
+resultado = 17 % 4 
+print(resultado)
+
+#2 
+
+b = (9 * 3) **2
+print(b)
+
+#3 
+
+c = 81 ** 0.5
+print(c)
+
+#4
+
+d =  20 + (3 * 4)
+print(d)
+
+#5
+
+e = (15 * 2) - 7 
+print(e)
+
+#6
+
+f = 5 ** 3 
+print(f)
+
+#7
+
+g = (17 + 21 + 25) / 3 
+print(g)
+
+#8
+
+h = (11 * 2) + 7 
+print(h)
+
+#9
+
+i = ((3 * 8) - 15) / 2
+print(i)
+
+#10
+
+j = 2 ** 10 
+print(j)
