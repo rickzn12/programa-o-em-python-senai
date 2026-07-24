d48f411f4614cc29c32b78bf6e8a41978aaa164b
rickzn12 <vieirarick08@gmail.com>
2026-07-23 21:22:58 -0300

    Add files via upload

---
+++ Aula-7/rick.py
@@ -0,0 +1,29 @@
+#0
+
+lista = list(range(2,21,2))
+print(lista)
+
+#1
+
+numeros1 = [1,2,3,4,5,6,7,8,9,10]
+print(numeros1)
+
+#2
+
+print(numeros1[2])
+print(numeros1)
+
+#3
+
+numeros1.append(9)
+print(numeros1)
+
+#4
+
+numeros1.pop(4)
+print(numeros1)
+
+#5
+
+carros = ["gol, fiat, commander"]
+print(carros, numeros1)
